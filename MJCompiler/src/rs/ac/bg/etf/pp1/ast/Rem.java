02a0ca21573ec617515516260e3063c47f1e754f
Filip Carevic <filip.carevic11@gmail.com>
2021-01-07 01:45:37 +0100

    ispravio bug za mjparser.java

--- MJCompiler/src/rs/ac/bg/etf/pp1/ast/Rem.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/ast/Rem.java
@@ -1,6 +1,6 @@
 // generated with ast extension for cup
 // version 0.8
-// 7/0/2021 1:24:51
+// 7/0/2021 1:43:47
 
 
 package rs.ac.bg.etf.pp1.ast;
--- MJCompiler/src/rs/ac/bg/etf/pp1/ast/StdConstChar.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/ast/StdConstChar.java
@@ -1,6 +1,6 @@
 // generated with ast extension for cup
 // version 0.8
-// 7/0/2021 1:24:50
+// 7/0/2021 1:43:46
 
 
 package rs.ac.bg.etf.pp1.ast;
